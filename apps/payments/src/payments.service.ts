555304e007720c859fc24a20fbad5b44ef43b6fc
Denis John <denis.john.ndarake@gmail.com>
2023-07-03 22:51:32 +0100

    Reservations Payments - part 2

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -14,18 +14,29 @@ export class PaymentsService {
 
   constructor(private readonly configService: ConfigService) {}
 
-  async createCharge({ card, amount }: CreateChargeDto) {
-    const paymentMethod = await this.stripe.paymentMethods.create({
-      type: 'card',
-      card,
-    });
+  // async createCharge({ card, amount }: CreateChargeDto) {
+  //   const paymentMethod = await this.stripe.paymentMethods.create({
+  //     type: 'card',
+  //     card,
+  //   });
 
+  //   const paymentIntent = await this.stripe.paymentIntents.create({
+  //     payment_method: paymentMethod.id,
+  //     amount: amount * 100,
+  //     confirm: true,
+  //     payment_method_types: ['card'],
+  //     currency: 'usd',
+  //   });
+
+  //   return paymentIntent;
+  // }
+
+  async createCharge({ amount }: CreateChargeDto) {
     const paymentIntent = await this.stripe.paymentIntents.create({
-      payment_method: paymentMethod.id,
       amount: amount * 100,
       confirm: true,
-      payment_method_types: ['card'],
       currency: 'usd',
+      payment_method: 'pm_card_visa',
     });
 
     return paymentIntent;
