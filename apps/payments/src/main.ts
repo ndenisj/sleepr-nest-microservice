38977e299dee0bf7745f4a95d477cea92873737c
Denis John <denis.john.ndarake@gmail.com>
2023-07-03 21:55:54 +0100

    new micro-service with stripe setup

---
+++ apps/payments/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { PaymentsModule } from './payments.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from 'nestjs-pino';
+import { Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.create(PaymentsModule);
+  app.useLogger(app.get(Logger));
+  const configService = app.get(ConfigService);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: configService.get('TCP_PORT'),
+    },
+  });
+  await app.startAllMicroservices();
+}
+bootstrap();
